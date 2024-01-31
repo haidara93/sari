88aae0410998abb74ced6229aa7b629437523ac8
haidara93 <haidara.yousef993@gmail.com>
2024-01-31 16:02:19 +0300

    add fcm config

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,7 +9,18 @@ import FirebaseCore
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     FirebaseApp.configure()
+    if #available(iOS 10.0, *) {
+        UNUserNotificationCenter.current().delegate = self
+    } else {
+        // Fallback on earlier versions
+    }
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+  @available(iOS 10.0, *)
+  override func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void)
+  {
+      completionHandler([.alert, .badge, .sound])
   }
 }
